de38ed978d22d6c0c50f27aff8a2378031e15f8a
Kayla Fang <kfang00@stuy.edu>
2019-03-06 11:06:26 -0500

    started bronze

---
+++ USACO.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class USACO {
+
+  public static int bronze(String filename) {
+    int[][] pasture;
+    File f = new File(filename);
+    Scanner in = new Scanner(f);
+	  while(in.hasNext()){
+		  h = in.nextLine();
+		  row += 1;
+		  col = h.length();
+		  st += h;
+    }
+  }
+
+  public static int silver(String filename) {
+
+  }
+}
