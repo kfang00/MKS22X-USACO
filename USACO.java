d54b0e72323b752bfa04a222dd55dcd7bfa8c862
Kayla Fang <kfang00@stuy.edu>
2019-03-07 11:01:44 -0500

    scanning the file

--- USACO.java
+++ USACO.java
@@ -4,11 +4,30 @@ import java.io.*;
 public class USACO {
 
   public static int bronze(String filename) {
-    int[][] pasture;
+    int[][] pasture, moves;
+    int[] i1 = new int[4];
     File f = new File(filename);
     Scanner in = new Scanner(f);
-	  while(in.hasNext()){
-		  h = in.nextLine();
+    for (int a = 0; a < 4; a++) {
+      i1[a] = Integer.parseInt(in.next());
+    }
+    int[][] pasture = new int[i1[0]][i1[1]];
+    for (int b = 0; b < i1[0]; b++) {
+      for (int c = 0; c < i1[1]; c++) {
+        pasture[b][c] = Integer.parseInt(in.next());
+      }
+    }
+    int[][] moves = new int[il[3]][3];
+    for (int d = 0; d < i1[0]; d++) {
+      for (int e = 0; e < i1[1]; e++) {
+        pasture[d][e] = Integer.parseInt(in.next());
+      }
+    }
+
+      }
+      else {
+
+      }
 		  row += 1;
 		  col = h.length();
 		  st += h;
