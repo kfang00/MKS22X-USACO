74127356f79c271bf8ec5500b2d98011d928cae7
Kayla Fang <kfang00@stuy.edu>
2019-03-10 16:34:17 -0400

    Finished bronze

--- Driver.java
+++ Driver.java
@@ -1,7 +1,14 @@
+import java.io.FileNotFoundException;
+
 public class Driver {
 
   public static void main(String[] args) {
-    USACO.bronze(makelake4.in);
+    try {
+      System.out.println(USACO.bronze("makelake.5.in"));
+    }
+    catch(FileNotFoundException e){
+      System.out.println("Invalid filename");
+    }
   }
 
 }
--- USACO.java
+++ USACO.java
@@ -3,11 +3,11 @@ import java.io.*;
 
 public class USACO {
 
-  public static int bronze(String filename) {
+  public static int bronze(String filename) throws FileNotFoundException{
     int[][] pasture, moves;
     int[] i1 = new int[4];
-    File f = new File(filename);
-    Scanner in = new Scanner(f);
+    File ff = new File(filename);
+    Scanner in = new Scanner(ff);
     for (int a = 0; a < 4; a++) {
       i1[a] = Integer.parseInt(in.next());
     }
@@ -20,16 +20,44 @@ public class USACO {
     moves = new int[i1[3]][3];
     for (int d = 0; d < i1[3]; d++) {
       for (int e = 0; e < 3; e++) {
-        pasture[d][e] = Integer.parseInt(in.next());
+        moves[d][e] = Integer.parseInt(in.next());
       }
     }
-    System.out.println(toString(pasture));
-    System.out.println(toString(moves));
-    return 0;
+
+    for (int f = 0; f < moves.length; f++) {
+      int max = 0;
+      for (int g = moves[f][0] - 1; g < moves[f][0] + 2; g++) {
+        for (int h = moves[f][1] - 1; h < moves[f][1] + 2; h++) {
+	  if (pasture[g][h] > max) {
+	    max = pasture[g][h];
+          }
+        }
+      }
+      for (int j = moves[f][0] - 1; j < moves[f][0] + 2; j++) {
+        for (int k = moves[f][1] - 1; k < moves[f][1] + 2; k++) {
+	  if (pasture[j][k] > (max - moves[f][2])) {
+	    pasture[j][k] = (max - moves[f][2]);
+          }
+        }
+      }
+    }
+    int sum = 0;
+    for (int m = 0; m < pasture.length; m++) {
+      for (int n = 0; n < pasture[0].length; n++) {
+	if (pasture[m][n] < i1[2]) {
+	  pasture[m][n] = (i1[2] - pasture[m][n]);
+	  sum += pasture[m][n];
+	}
+	else {
+	  pasture[m][n] = 0;
+	}
+      }
+    }
+    return (sum * 72 * 72);
   }
 
   public static int silver(String filename) {
-
+    return 0;
   }
 
   public static String toString(int[][] arr) {
